4385ed5f519602dc965e84ac3dcc22b2f45f1d75
Atha <athabarokahfc@gmail.com>
2024-06-28 21:38:03 +0700

    change route

--- api/payment.js
+++ api/payment.js
@@ -1,21 +1,18 @@
+// File: api/payment.js
 const express = require("express");
 const bodyParser = require("body-parser");
 const axios = require("axios");
-
 const app = express();
-const port = 3000;
 
 app.use(bodyParser.json());
 
-// Endpoint untuk handle payment
-app.post("/create-transaction", async (req, res) => {
-  const orderId = "order-id-dummy";
-  const grossAmount = 100000; // contoh jumlah nominal
+app.post("/api/payment", async (req, res) => {
+  const { order_id, gross_amount } = req.body;
 
   const transactionDetails = {
     transaction_details: {
-      order_id: orderId,
-      gross_amount: grossAmount,
+      order_id,
+      gross_amount,
     },
   };
 
@@ -43,6 +40,4 @@ app.post("/create-transaction", async (req, res) => {
   }
 });
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+module.exports = app;
